4d0466332f59b1be984072dacee50756cc76bf33
Guillem <guillem.sannicolas@keradgames.com>
2017-05-28 22:19:59 +0200

    controller tests

--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 RSpec.describe TransactionsController, type: :controller do
 
   describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    it "creates a new transaction with valid params" do
+      expect{
+        post :create,  :sender_id => 1, :receiver_id => 2, :money => 1000
+      }.to change(Transaction,:count).by(1)
+    end
+
+    it "doesn't creates a new transaction with invalid params" do
+      expect{
+        post :create,	 :receiver_id => 2, :money => 1000
+      }.to_not change(Transaction,:count)
     end
   end
 
